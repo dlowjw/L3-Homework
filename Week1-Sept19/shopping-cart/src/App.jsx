5c5f2a42d5f058d741100df80c91491970e5eddb
dlowjw <dlowjw@gmail.com>
2025-09-21 16:14:45 +0800

    Shopping Cart header

--- Week1-Sept19/shopping-cart/src/App.jsx
+++ Week1-Sept19/shopping-cart/src/App.jsx
@@ -1,33 +1,29 @@
+/**
+ * .map, .filter, .reduce
+ * build a shopping cart: 
+ * map -> displaying everything in shopping cart, 
+ * filter: filtering for specific items, 
+ * reduce: calculating total price in shopping cart
+
+Homework: 
+- practice rendering components and manipulating data with the map, reduce, and filter methods for arrays
+- rebuild either the todo app if you missed class or build a shopping cart app
+- note you can chain those methods inside of the jsx as well, in our class example we do it outside
+*/
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './components/header'
+// import { Cart } from './components/cart'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="">
+        <Header />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ Week1-Sept19/shopping-cart/src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h1 className="text-6xl">Cart</h1> 
+      <br />
+      <p className="text-3xl">You are eligible for free shipping </p>
+      <br />
+      <hr />
+    </div>
+  )
+}
+
+
