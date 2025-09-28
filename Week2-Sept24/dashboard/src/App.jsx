1daaef4fb589af74e6f2c3ab0d34573837060e11
dlowjw <dlowjw@gmail.com>
2025-09-28 20:54:57 +0800

    Install Axios + Get successful API Call test

--- Week2-Sept24/dashboard/src/App.jsx
+++ Week2-Sept24/dashboard/src/App.jsx
@@ -1,33 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import useFetchData from "./custom-hooks/useFetchData";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, error, getData] = useFetchData();
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* <button onClick={() => getData(url, key)}>Get Recipe</button> */}
+        <button onClick={() => getData("https://randomuser.me/api/", null)}>Get User</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ Week2-Sept24/dashboard/src/custom-hooks/useFetchData.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const useFetchData = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function getData(url, key) {
+    try {
+      const options = {
+        header: {
+          "x-api-key": key,
+          "Content-Type": "application.json",
+        },
+      };
+
+      const { data } = await axios.get(url, options);
+      setData(data);
+      console.log(data);
+    } catch (e) {
+      setError(e);
+    }
+  }
+
+  return [data, error, getData];
+};
+
+export default useFetchData;
