d6393269302c708aa82b52c5b41e79893ce8d3ad
dlowjw <dlowjw@gmail.com>
2025-09-29 21:48:21 +0800

    Add dashboard blueprint, add header WIP

--- Week2-Sept24/dashboard/src/App.jsx
+++ Week2-Sept24/dashboard/src/App.jsx
@@ -6,10 +6,14 @@ import './App.css'
  * Build a dashboard - due on Thursday
  * - Components, API Request
  * - useState, useEffect
- * - map, filter, reduce
+ * - map -> map recipe data 
+ * - filter -> by diet
+ * - reduce -> total calorie / nutrition data
  */
 
-const url = "https://api.spoonacular.com/recipes/random";
+// const url = "https://api.spoonacular.com/recipes/random";
+import { Header } from './components/header'
+const url = "https://api.spoonacular.com/recipes/complexSearch?diet=ketogenic&addRecipeNutrition=true";
 const key = "cc0e5594b35e41b3b74872e2971cf192";
 
 function App() {
@@ -18,6 +22,7 @@ function App() {
   return (
     <>
       <div>
+        <Header />
         <button onClick={() => getData(url, key)}>Get Recipe</button>
         {/* <button onClick={() => getData("https://randomuser.me/api/", null)}>Get User</button> */}
       </div>
---
+++ Week2-Sept24/dashboard/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="max-w-4/5">
+      <h1 className="text-6xl p-50">Your daily random meal</h1>
+      <hr />
+    </div>
+  )
+}
+
